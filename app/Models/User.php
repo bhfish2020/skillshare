d87750c487ed7f2f71f78213981602d25ae27390
bhfish2020 <bhfish2020@gmail.com>
2025-08-11 18:59:54 -0500

    Fix email domain check in canAccessPanel method to allow access for all '@robmurrer.com' addresses.

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, 'c@robmurrer.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@robmurrer.com') && $this->hasVerifiedEmail();
     }
 
     public function posts()
