eae063ff4cd38e327ab823b2c568dc6e9acaacf2
bhfish2020 <bhfish2020@gmail.com>
2025-08-11 19:02:24 -0500

    Update canAccessPanel method to always return true, removing email domain check.

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@robmurrer.com') && $this->hasVerifiedEmail();
+        return true;
     }
 
     public function posts()
